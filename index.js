f58e51adf463ca919d0cb7a20196e86c12d9b5fb
ruanthow <ruan.thow3@gmail.com>
2023-01-16 14:31:45 -0300

    mudando estrutura

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middleware = jsonServer.defaults();
-const router = jsonServer.router("./api/db.json")
+const router = jsonServer.router("./db.json")
 
 server.use(middleware)
 server.use(router)
